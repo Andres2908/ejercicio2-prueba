f75cb2836a89951e7f93b6fbe287457806dc4b49
Andres2908 <saavedratrejo24@gmail.com>
2022-09-03 17:47:44 -0500

    color

--- src/components/Botones.js
+++ src/components/Botones.js
@@ -2,6 +2,10 @@ import React, {useState} from "react";
 
 export function Botones() {
     const [count, setCount] = useState(0)
+    const [countColor, setCountColor] = useState({
+        fontSize: 40,
+        background: "red",
+    })
     const addCount = () => {
         setCount(count+1)
     }
@@ -9,21 +13,28 @@ export function Botones() {
         setCount(count-2)
     }
     const divideCount = () => {
+        backgroundCount(count % 3)
         setCount(count/3)
     }
     const resetCount = () => {
         setCount(0)
+        setCountColor({...countColor, background: "red"})
+    }
+    const backgroundCount = (modulo) => {
+        if(modulo === 0) setCountColor({...countColor, background: "red"})
+        else if(modulo === 1) setCountColor({...countColor, background: "green"})
+        else setCountColor({...countColor, background: "blue"})
     }
 
     let center = {textAlign: "center", paddingTop: 40} //css
-    
+
     return(<div style={center}>
             <button onClick={addCount}>Add</button>
             <button onClick={substracCount}>Substrac</button>
             <button onClick={divideCount}>Divide</button>
             <button onClick={resetCount}>Reset</button>
 
-            <p style={{fontSize: 40}}>{count}</p>
+            <p style={countColor}>{count}</p>
 
         </div>
     )
